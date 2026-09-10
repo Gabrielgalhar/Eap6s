4ad2dc250a9a28be611d9e2fa4353664d569c6a1
Gabriel Galhardone Felipe <galhardonefelipegabriel@gmail.com>
2026-09-10 09:02:13 -0300

    teste

---
+++ AEP6S-1-Entrega-main/agroPec/src/test/java/com/agroPec/agropec/AgroPecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroPec.agropec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroPecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
